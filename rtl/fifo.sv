a73b06c3101f33f633746e7d3ca4666a9e2acc6f
Someguy151 <jonathanhooper3@hotmail.com>
2026-07-03 17:33:03 -0700

    Implement parameterized FIFO module

--- rtl/fifo.sv
+++ rtl/fifo.sv
@@ -1,8 +1,40 @@
 module fifo #(
     // fifo parameters
+    parameter DATA_WIDTH = 8,
+    parameter FIFO_DEPTH = 16
+
 ) (
-    // fifo ports
+    // Fifo ports
+    // ----------------------------
+
+    // Clock and reset
+    input logic clk,
+    input logic rst_n,
+    
+    // Enable for both ports
+    input logic wr_en,
+    input logic rd_en,
+
+    // Input and output data bus
+    input logic  [DATA_WIDTH-1:0] din,
+    output logic [DATA_WIDTH-1:0] dout,
+
+    // Interface status outputs
+    output logic full,
+    output logic empty,
 
 );
+
+    // Time scale
+    timeunit 1ns; timeprecision 100ps;
+
+    // Pointer width
+    localparam ADDR_WIDTH = $clog2(FIFO_DEPTH);
+
+    // Internal state
+    logic [ADDR_WIDTH-1:0] wr_ptr;
+    logic [ADDR_WIDTH-1:0] rd_ptr; 
+
+
     
-endmodule
+endmodule : fifo
