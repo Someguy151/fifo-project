952d261e35bd1aaf052c690504c8d61e48a295a3
Someguy151 <jonathanhooper3@hotmail.com>
2026-07-06 19:05:01 -0700

    Basic testbench framework

--- tb/fifo_tb.sv
+++ tb/fifo_tb.sv
@@ -1,1 +1,29 @@
-// fifo testbench
+`timescale 1ns/100ps
+
+module fifo_tb;
+
+    parameter DATA_WIDTH = 8;
+    parameter FIFO_DEPTH = 16;
+
+    // DUT Signals -------------------------
+    // Clock and reset
+    logic clk,
+    logic rst_n,
+    // Enable for both ports
+    logic wr_en,
+    logic rd_en,
+    // Input and output data bus
+    logic [DATA_WIDTH-1:0] din,
+    logic [DATA_WIDTH-1:0] dout,
+    // FIFO status flags
+    logic full,
+    logic empty
+
+    // FIFO DUT instantiation 
+    fifo #(
+        .DATA_WIDTH(DATA_WIDTH),
+        .FIFO_DEPTH(FIFO_DEPTH)
+    ) dut ( );
+
+    
+endmodule
